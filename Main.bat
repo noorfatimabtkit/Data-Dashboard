8931423ef50c0db34af451728e82b1b77e7f4b2a
noorfatimabtkit <noorfatima.btik@gmail.com>
2024-09-25 16:12:00 +0500

    .

--- Main.bat
+++ Main.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 
 echo Checking if "Processed data.json" already exists...
@@ -24,30 +23,3 @@ IF EXIST "Processed data.json" (
 )
 
 pause
-=======
-@echo off
-
-echo Checking if "Processed data.json" already exists...
-IF EXIST "Processed data.json" (
-    echo "Processed data.json" already exists. Skipping the DataProcessor script.
-) ELSE (
-    echo "Processed data.json" not found. Running the DataProcessor script...
-    python DataProcessor.py
-    echo DataProcessor script finished.
-)
-
-echo Checking if "Processed data.json" exists after the DataProcessor script...
-IF EXIST "Processed data.json" (
-    echo "Processed data.json" found. Running the Dashboard script...
-    python Dashboard.py
-    echo Dashboard script finished.
-    
-    echo Launching the dashboard using Streamlit...
-    streamlit run Dashboard.py
-) ELSE (
-    echo "Processed data.json" not found. Skipping the Dashboard script.
-    pause
-)
-
-pause
->>>>>>> 222f1dee3f04cf95e7eb12c66294b2f7efee6525
